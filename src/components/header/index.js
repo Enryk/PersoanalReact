e7098e4a0e55361c84ef94fe1819f9530de18b7f
Carlos Enryk <carlosenryk@hotmail.com>
2022-10-24 17:41:32 -0300

    Importacao React e criação de components

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  './style.css'
+
+const Header = () => {
+    return (
+        <aside>
+            <span className="MenuLabel">VINICIUS ÁVILA PERSONAL TRAINER</span>
+        </aside>
+    );
+}
+
+export default Header;
---
+++ src/components/nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import  './style.css'
+
+const TextNav = () => {
+    return (
+        <nav className="NavText NavImg">
+            <h2 className="msgInicial typing">Conheça</h2>
+        </nav>
+    );
+}
+
+export default TextNav;
