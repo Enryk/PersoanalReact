f38ed62248d1f292569fff8330013a94c31618bf
Carlos Enryk <carlosenryk@hotmail.com>
2022-10-28 14:26:58 -0300

    buttons

--- src/buttons/index.js
+++ src/buttons/index.js
@@ -3,16 +3,16 @@ import  './style.css'
 
 function Botao() {
   return (
-    <section className="section">
-        <header className="linkPag">
+    <section className="sectionbutton">
+        <article className="articlebutton linkPag">
             <div className="btn">
-                <button><a className='link' href="https://wa.me/5535999772226?text=Olá,%20eu%20gostaria%20de%20marcar%20uma%20avaliação%20com%20você.">Exercite em Casa</a></button>
+                <button><a className='linkbtn' href="https://wa.me/5535999772226?text=Olá,%20eu%20gostaria%20de%20marcar%20uma%20avaliação%20com%20você.">Exercite em Casa</a></button>
                 
-                <button><a className='link' href="https://wa.me/5535999772226?text=Olá,%20eu%20gostaria%20de%20marcar%20uma%20avaliação%20com%20você.">Calcule seu IMC</a></button>
+                <button><a className='linkbtn' href="https://wa.me/5535999772226?text=Olá,%20eu%20gostaria%20de%20marcar%20uma%20avaliação%20com%20você.">Calcule seu IMC</a></button>
                 
-                <button><a className='link' href="https://wa.me/5535999772226?text=Olá,%20eu%20gostaria%20de%20marcar%20uma%20avaliação%20com%20você.">Contactar Personal</a></button>
+                <button><a className='linkbtn' href="https://wa.me/5535999772226?text=Olá,%20eu%20gostaria%20de%20marcar%20uma%20avaliação%20com%20você.">Contactar Personal</a></button>
             </div>
-        </header>
+        </article>
     </section>
   );
 }
