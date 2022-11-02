04219b61279e035c9c3fdbd2643b158bb111757a
Carlos Enryk <carlosenryk@hotmail.com>
2022-11-02 19:20:56 -0300

    pag de contato

---
+++ src/components/pag_contato/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import TextExercicios from '../../shared/exercicios';
+import Header from '../../shared/header';
+
+
+function Exercicios() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default Exercicios;
